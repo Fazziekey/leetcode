098f6d8172636af0c9401c3505295325a50a5dbd
Fazzie <1240419984@qq.com>
2021-03-07 15:55:56 +0800

    day3

--- 03/03.cpp
+++ 03/03.cpp
@@ -1,4 +1,34 @@
 # include <iostream>
 int main(){
-    
+
 }
+
+class Solution {
+public:
+    int findRepeatNumber(vector<int>& nums) {
+        unordered_map<int, bool> map;
+        for(int num : nums) {
+            if(map[num]) return num;
+            map[num] = true;
+        }
+        return -1;
+    }
+};
+
+class Solution {
+public:
+    int findRepeatNumber(vector<int>& nums) {
+        int i = 0;
+        while(i < nums.size()) {
+            if(nums[i] == i) {
+                i++;
+                continue;
+            }
+            if(nums[nums[i]] == nums[i])
+                return nums[i];
+            swap(nums[i],nums[nums[i]]);
+        }
+        return -1;                                                                     
+    }
+};
+
