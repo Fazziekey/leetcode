67f6c0d0c943b26ca5332caf17106e0e70e0f476
马千里 <maqianli@CN0314000316M.local>
2022-03-03 14:53:58 +0800

    3.3

---
+++ Array/258各位相加.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum = 0;
+        do{
+            sum = 0;
+            while(num > 0){
+                sum += (num%10);
+                num /= 10;
+            }
+            num = sum;
+        }while(sum > 9);
+
+        return sum;
+    }
+};
