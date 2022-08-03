4fcc8170f9faf5b3f021d10983df9cf1b3ce25a3
Fazzie <1240419984@qq.com>
2022-08-03 11:57:49 +0800

    sg

--- meituan.cpp
+++ meituan.cpp
@@ -7,8 +7,21 @@ using namespace std;
 
 
 int main(){
-    
-    string s;
-    s.push_back(1);
-    cout << s;
+    int year;
+    cin >> year;
+    if(year % 400 == 0){
+        cout << "yes";
+        return 0;
+    }
+    else if(year % 100 == 0){
+        cout << "no";
+        return 0;
+    }else if(year % 4 == 0){
+        cout << "yes";
+        return 0;
+    }else{
+        cout << "no";
+        return 0; 
+    }
+    return 0;
 }
