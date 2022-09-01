b6149aca56c6ab010ce967bae02695d0b74b4b7b
Fazzie <1240419984@qq.com>
2022-09-01 09:43:35 +0800

    9.1

--- stack/1475商品折扣后的最终价格.cpp
+++ stack/1475商品折扣后的最终价格.cpp
@@ -4,15 +4,6 @@
 
 // 请你返回一个数组，数组中第 i 个元素是折扣后你购买商品 i 最终需要支付的价格。
 
-// 输入：prices = [8,4,6,2,3]
-// 输出：[4,2,4,2,3]
-// 解释：
-// 商品 0 的价格为 price[0]=8 ，你将得到 prices[1]=4 的折扣，所以最终价格为 8 - 4 = 4 。
-// 商品 1 的价格为 price[1]=4 ，你将得到 prices[3]=2 的折扣，所以最终价格为 4 - 2 = 2 。
-// 商品 2 的价格为 price[2]=6 ，你将得到 prices[3]=2 的折扣，所以最终价格为 6 - 2 = 4 。
-// 商品 3 和 4 都没有折扣。
-
-
 // 来源：力扣（LeetCode）
 // 链接：https://leetcode.cn/problems/final-prices-with-a-special-discount-in-a-shop
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -29,8 +20,7 @@ public:
             ans[i] = prices[i];
             for(int j = i + 1; j < size; j++) {
                 if(prices[j] <= prices[i]) {
-                    ans[i] = prices[i] - prices[j];
-                    break;
+                    ans[i] = min(ans[i],(prices[i] - prices[j]));
                 }
             }
         }
