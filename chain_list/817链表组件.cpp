fbf501a6d4d7fa0e204981ba4d82370357e6ad27
Fazzie <1240419984@qq.com>
2022-10-12 10:10:15 +0800

    10.12

---
+++ chain_list/817链表组件.cpp
@@ -0,0 +1,30 @@
+// 给定链表头结点 head，该链表上的每个结点都有一个 唯一的整型值 。同时给定列表 nums，该列表是上述链表中整型值的一个子集。
+
+// 返回列表 nums 中组件的个数，这里对组件的定义为：链表中一段最长连续结点的值（该值必须在列表 nums 中）构成的集合。
+
+ /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int numComponents(ListNode* head, vector<int>& nums) {
+        unordered_set<int> s;
+        for(auto n:nums) s.insert(n);
+        int res = 0;
+        auto p = head;
+        while (p)
+        {
+            if(s.count(p->val) && (p->next == nullptr || !s.count(p->next->val))) res++;
+            p = p->next;
+        }
+
+        return res;
+    }
+};
