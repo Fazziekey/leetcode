06b4eab93bcc6368de9c1f86ca919c218be4033a
Fazzie <1240419984@qq.com>
2023-01-31 09:51:48 +0800

    2.1

---
+++ Array/2319判断矩阵是否是一个 X 矩阵.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
+        for(int i = 0;i < row;i++){
+            for(int j = 0;j < col;j++){
+                if(i == j || i + j == col - 1){
+                    if(grid[i][j] == 0){
+                        return false;
+                    }
+                }
+                else{
+                    if(grid[i][j] != 0){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
